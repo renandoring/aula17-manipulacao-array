a58b0142a51eb44852cce37c7043213b16e1398a
rdoring <rdoring@rdoring.(none)>
2014-03-01 19:55:38 -0300

    aula17

---
+++ Material Suplementar/Aula 17/Index.php
@@ -0,0 +1,17 @@
+﻿<?php
+	// Cria uma vetor contendo quatro nomes de pessoas
+	$nomes[] = 'Maria';
+	$nomes[] = 'José';
+	$nomes[] = 'Pedro';
+	$nomes[] = 'Paulo';
+	
+	// Percorre todo o vetor e exibe cada valor armazenado no mesmo
+	foreach($nomes as $valor){
+		echo $valor.'<br>';
+	}
+	// Cria um variável para armazenar a contagem de posições do vetor
+	$contagem = count($nomes);
+	// Exibe uma mensagem na tela contendo o valor da contagem de posições
+	echo 'O Array contém '.$contagem.' Posições';
+
+?>
